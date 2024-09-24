f96a85e475fcdf82af186255c67a4c62288b64bf
Ira <iramladanovych@gmail.com>
2024-09-24 19:44:39 +0300

    Removed static files from repository

--- cafe_kitchen_service/settings.py
+++ cafe_kitchen_service/settings.py
@@ -101,11 +101,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, "static")
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Директорія для зібраних файлів
+
 
 ASSETS_ROOT = os.getenv("ASSETS_ROOT", '/static/assets')
 
