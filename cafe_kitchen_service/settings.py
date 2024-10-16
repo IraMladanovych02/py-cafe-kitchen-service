4e2a90d18268f3fef1c4da73b1d91fd7bf1e42fe
Ira <iramladanovych@gmail.com>
2024-10-16 21:22:26 +0300

    Added py-cafe-kitchen-service-1.onrender.com to ALLOWED HOSTS

--- cafe_kitchen_service/settings.py
+++ cafe_kitchen_service/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "py-cafe-kitchen-service-1.onrender.com"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
